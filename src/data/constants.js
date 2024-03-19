e3da28ec9842e27b397d0d2100b2850386875f19
Ninos <ninos.dinkha97@gmail.com>
2024-03-19 22:59:39 +0300

    created fetchData function with bearer token

---
+++ src/data/constants.js
@@ -0,0 +1,5 @@
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3/"
+export const BearerToken =
+"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYTFkZDVhZWUxY2FiZWNkNzQzOGY3YzRiOGE2YzBjOCIsInN1YiI6IjY1ZjhjZTIwNGI5YmFlMDE4MzdkODc3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GYLuOnQ1z3JfjiJSeUrFy0nzMh9CXx3NMMWW3osS7nQ"
+
+// const myBearerToken = process.env.NEXT_PUBLIC_BEARER_TOKEN
