5ba03f909b15a919a9ce71b9d86b700340ebadd9
elaf <elafghassangardi@gmail.com>
2024-03-20 22:45:17 +0300

    fetched actors data and send it to Card component

---
+++ src/components/Actors.jsx
@@ -0,0 +1,30 @@
+"use client"
+import React, {useState, useEffect} from "react";
+import { fetchData } from "@/_utils/fetchData";
+import Cards from "./Cards"
+
+function Actors(){
+    const [gitActors, setGitActors] = useState([])
+  
+    useEffect(() => {
+        const fetchAllActorsInfo = async () => {
+          try {
+            const data = await fetchData('person/popular')
+            setGitActors(data.results)
+          } catch (error) {
+            console.error('Error fetching movies:', error)
+          }
+        }
+    
+        fetchAllActorsInfo()
+      }, [])
+
+    return (
+        <div className="flex flex-row">
+            <div className="w-64 bg-teal-500"></div> {/** replace with sideBar component*/} 
+            <Cards actors={gitActors} />
+        </div>
+    )
+}
+
+export default Actors;
