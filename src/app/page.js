83a68346b137587ae70251dbfd88c718d86bec2b
elaf <elafghassangardi@gmail.com>
2024-03-20 22:44:25 +0300

    fixed merge components from main

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import React from "react"
+import Actors from "@/components/Actors"
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center py-36">
-      <h1 className="text-blue-500 font-bold">Hello World</h1>
+    <div>
+      <Actors/>
     </div>
   )
 }
