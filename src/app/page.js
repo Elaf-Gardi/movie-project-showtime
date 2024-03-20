94826a8d44ce088bfd1fe3d2245f2c500e1dade5
Nooralhussein <lighthero212@gmail.com>
2024-03-20 21:45:38 +0300

    Add movies page with category selection and moviecards component

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
 import Test from "@/components/Test"
 
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div className="flex flex-col space-y-5 justify-center items-center py-36">
+      <Link href='movies'> movies </Link>
       <Test />
     </div>
   )
