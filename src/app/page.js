6267e8da96bae69a5cfd45acfe83bb767101fe14
unknown <afyaakhudur@gmail.com>
2024-03-20 16:42:05 +0300

    initial side navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import Test from "@/components/Test"
+import NavBar from "@/components/navbar/NavBar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col space-y-5 justify-center items-center py-36">
-      <Test />
+    <div >
+     <NavBar/>
     </div>
   )
 }
