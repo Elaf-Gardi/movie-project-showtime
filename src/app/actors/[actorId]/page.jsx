eb6722b13cde472b8501aeec4ae33d79882cb809
Ninos <ninos.dinkha97@gmail.com>
2024-03-21 16:28:36 +0300

    updated Actor Info page

--- src/app/actors/[actorId]/page.jsx
+++ src/app/actors/[actorId]/page.jsx
@@ -1,16 +1,29 @@
-const page = async ({ params }) => {
-  const actorId= params.id;
-  const res = await fetch(
-    `https://api.themoviedb.org/3/person/${actorId}`
-  );
-  const data = await res.json();
+import { fetchData } from '@/_utils/fetchData'
 
-return (
-  <div>
-    <h1>{data.name}</h1>
-  </div>
-)
+const ActorInfo = async ({ params }) => {
+  const { actorId } = params
+  const actorInfo = await fetchData(`/person/${actorId}`)
+  console.log(actorInfo)
 
+  return (
+    <div className="container mx-auto py-4">
+      <div className="max-w-xl mx-auto">
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${actorInfo.profile_path}`}
+          alt={actorInfo.name}
+          className="w-44 h-44 rounded-lg shadow-lg"
+        />
+        <h1 className="mt-4 text-2xl font-bold">{actorInfo.name}</h1>
+        <p className="mt-2 text-gray-600">{actorInfo.birthday}</p>
+        <p className="mt-2">{actorInfo.biography}</p>
+        <ul className="mt-4">
+          {actorInfo.also_known_as.map((name, index) => (
+            <li key={index}>{name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
 }
 
-export default page;
+export default ActorInfo
