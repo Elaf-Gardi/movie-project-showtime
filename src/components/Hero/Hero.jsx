12f15d12839fa78166b30c1c2336e08d6565ad14
elaf <elafghassangardi@gmail.com>
2024-03-24 21:23:05 +0300

    style the fonts and add a hover to cards

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -59,10 +59,11 @@ const HeroSection = () => {
         />
 
         {latestMovies.length > 0 && (
-          <div className="w-[600px] py-10 sm:py-14 sm:px-10 bg-blue-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 border border-gray-100/10 cursor-grabbing">
+          <div className="w-[600px] py-10 sm:py-14 sm:px-10  bg-blue-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 border border-gray-100/10 cursor-grabbing">
             <Carousel {...carouselConfig}>
               {latestMovies.slice(1).map((movie) => (
-                <img
+                <div className='hover:scale-110 transition-all duration-300 ease-in-out'>
+                   <img
                   key={movie.id}
                   src={`${TMDB_IMAGE_BASE_URL}w500${movie.poster_path}`}
                   alt={movie.title}
@@ -70,6 +71,8 @@ const HeroSection = () => {
                   onClick={() => handleMovieClick(movie)}
                   draggable="false"
                 />
+                </div>
+               
               ))}
             </Carousel>
           </div>
