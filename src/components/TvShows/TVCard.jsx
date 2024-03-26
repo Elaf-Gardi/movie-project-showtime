cb17de7185f193d0e27b2702217c40c2b73c5be6
elaf <elafghassangardi@gmail.com>
2024-03-26 19:27:02 +0300

    final

--- src/components/TvShows/TVCard.jsx
+++ src/components/TvShows/TVCard.jsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { TMDB_IMAGE_BASE_URL } from '@/data/constants'
 
 const TvCard = ({ tvShow }) => {
+
   const { id, name, poster_path, first_air_date, overview, vote_average } =
     tvShow
   const formattedDate = new Date(first_air_date).getFullYear()
@@ -12,12 +13,12 @@ const TvCard = ({ tvShow }) => {
   return (
     <Link href={`/shows/${id}`} passHref>
       <div className="relative w-52 h-80 rounded-2xl transition-transform hover:scale-110 shadow-inner">
-        <img
-          src={`${TMDB_IMAGE_BASE_URL}w500${poster_path}`}
+      <img
+          src={poster_path ? `${TMDB_IMAGE_BASE_URL}w500${poster_path}` : '/showtime-logo-yellow.png'}
           alt={`Poster of ${name}`}
-          className="absolute top-0 left-0 w-full h-full object-cover rounded-2xl z-10"
+          className="absolute top-0 left-0 w-full h-full object-cover rounded-2xl cursor-pointer z-10"
         />
-        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-30 transition-opacity duration-300 z-20 hover:opacity-0"></div>
+        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-30 rounded-2xl transition-opacity duration-300 z-20 hover:opacity-0"></div>
         <div className="absolute top-0 left-0 bg-yellow-400 text-black rounded-br-lg px-2 py-1 text-sm font-bold z-20">
           {formattedRating}
         </div>
