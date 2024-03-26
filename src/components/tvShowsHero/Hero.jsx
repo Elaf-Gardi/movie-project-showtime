4e263b338fea423a6a55ef4228ca1459d035f3e6
Ninos <ninos.dinkha97@gmail.com>
2024-03-26 15:16:39 +0300

    added logic to display genres in the hero

--- src/components/tvShowsHero/Hero.jsx
+++ src/components/tvShowsHero/Hero.jsx
@@ -6,73 +6,62 @@ import 'react-multi-carousel/lib/styles.css'
 import BackgroundImage from './BackgroundImage'
 import { fetchData } from '@/_utils/fetchData'
 import { heroCarouselConfig } from '@/data/heroCarouselConfig'
-import MovieDetails from './MovieDetails'
 import { TMDB_IMAGE_BASE_URL } from '@/data/constants'
+import ShowDetails from './ShowDetails'
 
-const HeroSection = () => {
-  const [latestMovies, setLatestMovies] = useState([])
-  const [selectedMovie, setSelectedMovie] = useState(null)
-  const [movieDetails, setMovieDetails] = useState(null)
+const HeroSection = ({ shows }) => {
+  const [selectedShow, setSelectedShow] = useState(null)
+  const [showDetails, setShowDetails] = useState(null)
 
   useEffect(() => {
-    const fetchMovieData = async () => {
-      try {
-        const movieData = await fetchData('/tv/popular')
-        setLatestMovies(movieData.results)
+    if (shows.length > 0) {
+      setSelectedShow(shows[0])
+    }
+  }, [shows])
 
-        // Fetch details for the first movie right away
-        const firstMovieId = movieData.results[0].id
-        const additionalDetails = await fetchData(`/tv/${firstMovieId}`)
-        setSelectedMovie(movieData.results[0])
-        setMovieDetails(additionalDetails)
-      } catch (error) {
-        console.error('Error fetching movie data:', error)
+  const handleShowClick = async (show) => {
+    setSelectedShow(show)
+  }
+
+  useEffect(() => {
+    const fetchShowDetails = async () => {
+      if (selectedShow) {
+        try {
+          const details = await fetchData(`/tv/${selectedShow.id}`)
+          setShowDetails(details)
+        } catch (error) {
+          console.error('Error fetching show details:', error)
+        }
       }
     }
 
-    fetchMovieData()
-  }, [])
-
-  const handleMovieClick = async (movie) => {
-    setSelectedMovie(movie)
-
-    try {
-      const movieId = movie.id
-      const details = await fetchData(`/tv/${movieId}`)
-      setMovieDetails(details)
-    } catch (error) {
-      console.error('Error fetching movie details:', error)
-    }
-  }
-
+    fetchShowDetails()
+  }, [selectedShow])
   return (
     <section className="relative overflow-hidden h-[100dvh]">
-      <BackgroundImage selectedMovie={selectedMovie} />
+      <BackgroundImage selectedShow={selectedShow} />
 
       <div
         className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-black/70 bg-fixed 
         flex flex-col lg:flex-row sm:items-center sm:justify-center gap-x-20 gap-y-44 px-4 pt-20 md:pt-10"
       >
-        <MovieDetails
-          selectedMovie={selectedMovie}
-          movieDetails={movieDetails}
-        />
+        <ShowDetails selectedShow={selectedShow} showDetails={showDetails} />
 
-        {latestMovies.length > 0 && (
+        {shows.length > 0 && (
           <div className="w-[600px] py-10 sm:py-14 sm:px-10  bg-blue-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 border border-gray-100/10 cursor-grabbing">
             <Carousel {...heroCarouselConfig}>
-              {latestMovies.map((movie) => (
+              {shows.map((show) => (
                 <div
-                  key={movie.id}
+                  key={show.id}
                   className="hover:scale-110 transition-all duration-300 ease-in-out"
                 >
                   <img
-                    src={`${TMDB_IMAGE_BASE_URL}w500${movie.poster_path}`}
-                    alt={movie.name}
+                    src={`${TMDB_IMAGE_BASE_URL}w500${show.poster_path}`}
+                    alt={show.name}
                     className={`w-44 h-52 cursor-pointer !self-center ${
-                      movie.id === selectedMovie?.id ? '!h-64 !w-48' : 'mt-6'
+                      show.id === selectedShow?.id ? '!h-64 !w-48' : 'mt-6'
                     }`}
-                    onClick={() => handleMovieClick(movie)}
+                    onClick={() => handleShowClick(show)}
                     draggable="false"
                   />
                 </div>
